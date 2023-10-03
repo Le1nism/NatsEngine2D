bac890fd4ba26f76e5f88d02c8ab36017d4f26d9
Appetible <cristian.capiferri@gmail.com>
2023-10-03 23:55:16 +0200

    Implemented batch rendering

--- src/main/java/jade/Scene.java
+++ src/main/java/jade/Scene.java
@@ -3,8 +3,11 @@ package jade;
 import java.util.ArrayList;
 import java.util.List;
 
+import renderer.Renderer;
+
 public abstract class Scene {
     
+    protected Renderer renderer = new Renderer();
     protected Camera camera;
     private boolean isRunning = false;
     protected List<GameObject> gameObjects = new ArrayList<>();
@@ -24,6 +27,7 @@ public abstract class Scene {
         for (GameObject go: gameObjects) {
 
             go.start();
+            this.renderer.add(go);
         }
 
         isRunning = true;
@@ -38,8 +42,14 @@ public abstract class Scene {
 
             gameObjects.add(go);
             go.start();
+            this.renderer.add(go);
         }
     }
 
     public abstract void update(float dt);
+
+    public Camera camera() {
+
+        return this.camera;
+    }
 }
--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -70,6 +70,11 @@ public class Window {
         return Window.window;
     }
 
+    public static Scene getScene() {
+
+        return get().currentScene;
+    }
+
     public void run() {
 
         System.out.println("Hello LWGJL " + Version.getVersion() + "!");
