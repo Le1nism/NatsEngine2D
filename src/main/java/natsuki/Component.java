dfed58ece62095cead47c0f32cb47df37ad8b7d4
Appetible <cristian.capiferri@gmail.com>
2023-10-08 12:18:09 +0200

    Exposed variables to level editor

--- src/main/java/natsuki/Component.java
+++ src/main/java/natsuki/Component.java
@@ -3,6 +3,9 @@ package natsuki;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
+import org.joml.Vector3f;
+import org.joml.Vector4f;
+
 import imgui.ImGui;
 
 public abstract class Component {
@@ -27,10 +30,13 @@ public abstract class Component {
 
             for (Field field : fields) {
 
+                boolean isTransient = Modifier.isTransient(field.getModifiers());
                 boolean isPrivate = Modifier.isPrivate(field.getModifiers());
 
                 if (isPrivate)
                     field.setAccessible(true);
+                
+                if (isTransient) continue;
 
                 Class type = field.getType();
                 Object value = field.get(this);
@@ -43,6 +49,37 @@ public abstract class Component {
 
                     if (ImGui.dragInt(name + ": ", imInt))
                         field.set(this, imInt[0]);
+                }
+                else if (type == float.class) {
+
+                    float val = (float)value;
+                    float[] imFloat = {val};
+
+                    if (ImGui.dragFloat(name + ": ", imFloat))
+                        field.set(this, imFloat[0]);
+                }
+                else if (type == boolean.class) {
+
+                    boolean val = (boolean)value;
+
+                    if (ImGui.checkbox(name + ": ", val))
+                        field.set(this, !val);
+                }
+                else if (type == Vector3f.class) {
+
+                    Vector3f val = (Vector3f)value;
+                    float[] imVec = {val.x, val.y, val.z};
+
+                    if (ImGui.dragFloat3(name + ": ", imVec))
+                        val.set(imVec[0], imVec[1], imVec[2]);
+                }
+                else if (type == Vector4f.class) {
+
+                    Vector4f val = (Vector4f)value;
+                    float[] imVec = {val.x, val.y, val.z, val.w};
+
+                    if (ImGui.dragFloat4(name + ": ", imVec))
+                        val.set(imVec[0], imVec[1], imVec[2], imVec[3]);
                 }
 
                 if (isPrivate)
