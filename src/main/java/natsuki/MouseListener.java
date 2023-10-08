6c6c4045b89156459fd1e2c679987f057092524d
Appetible <cristian.capiferri@gmail.com>
2023-10-08 16:47:40 +0200

    Fixed function to use screen height and not length

--- src/main/java/natsuki/MouseListener.java
+++ src/main/java/natsuki/MouseListener.java
@@ -101,7 +101,7 @@ public class MouseListener {
     public static float getOrthoY() {
 
         float currentY = getY();
-        currentY = (currentY / (float)Window.getWidth()) * 2.0f - 1.0f;
+        currentY = (currentY / (float)Window.getHeight()) * 2.0f - 1.0f;
 
         Vector4f tmp = new Vector4f(0, currentY, 0, 1);
         tmp.mul(Window.getScene().camera().getInverseProjection()).mul(Window.getScene().camera().getInverseView());
