3449f7cfe0527af1eb405e60f90f439398823256
Appetible <cristian.capiferri@gmail.com>
2023-11-14 17:40:02 +0100

    Gizmo arrows change color when hovering

--- src/main/java/components/TranslateGizmo.java
+++ src/main/java/components/TranslateGizmo.java
@@ -11,10 +11,10 @@ import natsuki.Window;
 
 public class TranslateGizmo extends Component {
 
-    private Vector4f xAxisColor = new Vector4f(1, 0, 0, 1);
-    private Vector4f xAxisColorHover = new Vector4f();
-    private Vector4f yAxisColor = new Vector4f(0, 1, 0, 1);
-    private Vector4f yAxisColorHover = new Vector4f();
+    private Vector4f xAxisColor = new Vector4f(1, 0.3f, 0.3f, 1);
+    private Vector4f xAxisColorHover = new Vector4f(1, 0, 0, 1);
+    private Vector4f yAxisColor = new Vector4f(0.3f, 1, 0.3f, 1);
+    private Vector4f yAxisColorHover = new Vector4f(0, 1, 0, 1);
 
     private GameObject xAxisObject;
     private GameObject yAxisObject;
@@ -24,6 +24,9 @@ public class TranslateGizmo extends Component {
 
     private Vector2f xAxisOffset = new Vector2f(64, -5);
     private Vector2f yAxisOffset = new Vector2f(16, 61);
+
+    private int gizmoWidth = 16;
+    private int gizmoHeight = 48;
 
     private PropertiesWindow propertiesWindow;
 
@@ -53,6 +56,17 @@ public class TranslateGizmo extends Component {
     @Override
     public void update(float dt) {
 
+        this.activeGameObject = this.propertiesWindow.getActiveGameObject();
+        if (this.activeGameObject != null) this.setActive();
+        else {
+
+            this.setInactive();
+            return;
+        }
+
+        boolean xAxisHot = checkXHoverState();
+        boolean yAxisHot = checkYHoverState();
+
         if (this.activeGameObject != null) {
 
             this.xAxisObject.transform.position.set(this.activeGameObject.transform.position);
@@ -60,10 +74,6 @@ public class TranslateGizmo extends Component {
             this.xAxisObject.transform.position.add(this.xAxisOffset);
             this.yAxisObject.transform.position.add(this.yAxisOffset);
         }
-
-        this.activeGameObject = this.propertiesWindow.getActiveGameObject();
-        if (this.activeGameObject != null) this.setActive();
-        else this.setInactive();
     }
 
     private void setActive() {
@@ -77,5 +87,31 @@ public class TranslateGizmo extends Component {
         this.activeGameObject = null;
         this.xAxisSprite.setColor(new Vector4f(0, 0, 0, 0));
         this.yAxisSprite.setColor(new Vector4f(0, 0, 0, 0));
-    } 
+    }
+
+    private boolean checkXHoverState() {
+
+        Vector2f mousePos = new Vector2f(MouseListener.getOrthoX(), MouseListener.getOrthoY());
+        if (mousePos.x <= xAxisObject.transform.position.x && mousePos.x >= xAxisObject.transform.position.x - gizmoHeight && mousePos.y >= xAxisObject.transform.position.y && mousePos.y <= xAxisObject.transform.position.y + gizmoWidth) {
+
+            xAxisSprite.setColor(xAxisColorHover);
+            return true;
+        }
+
+        xAxisSprite.setColor(xAxisColor);
+        return false;
+    }
+
+    private boolean checkYHoverState() {
+
+        Vector2f mousePos = new Vector2f(MouseListener.getOrthoX(), MouseListener.getOrthoY());
+        if (mousePos.x <= yAxisObject.transform.position.x && mousePos.x >= yAxisObject.transform.position.x - gizmoWidth && mousePos.y <= yAxisObject.transform.position.y && mousePos.y >= yAxisObject.transform.position.y - gizmoHeight) {
+
+            yAxisSprite.setColor(yAxisColorHover);
+            return true;
+        }
+
+        yAxisSprite.setColor(yAxisColor);
+        return false;
+    }
 }
